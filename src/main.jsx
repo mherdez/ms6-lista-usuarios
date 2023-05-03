cfa942ca03ab8a0ae320a71e5786c2f60b889b75
Herdez <mherdez@users.noreply.github.com>
2023-05-03 10:57:47 -0600

    crud

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// import './index.css'
 import Intro from './Intro'
+import Todo from './components/Todo';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
-    <App />
+    // <App />
+    <Todo />
 )
